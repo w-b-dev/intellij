f62679f8663f8e5e6a06b5870f1c1a55f88a4792
GitHub Action <action@github.com>
2021-12-30 15:32:43 +0000

    Template cleanup

--- src/main/kotlin/com/github/wbdev/intellij/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/wbdev/intellij/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.wbdev.intellij.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wbdev.intellij.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/wbdev/intellij/services/MyProjectService.kt
+++ src/main/kotlin/com/github/wbdev/intellij/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.wbdev.intellij.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wbdev.intellij.MyBundle
 
 class MyProjectService(project: Project) {
 
